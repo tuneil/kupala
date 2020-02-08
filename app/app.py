de45971b1daf2de5eabf53ea44a41e790632f4b1
Andrii Rykhlivskyi <dumpandrew@gmail.com>
2020-02-08 00:59:55 +0000

    Add default values for telemetry data

--- app/app.py
+++ app/app.py
@@ -9,10 +9,15 @@ i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
 
 def publish():
     while True:
-        bme = bme280.BME280(i2c=i2c)
-        temp = bme.temperature
-        hum = bme.humidity
-        pres = bme.pressure
+        try:
+            bme = bme280.BME280(i2c=i2c)
+            temp = bme.temperature
+            hum = bme.humidity
+            pres = bme.pressure
+        except:
+            temp = NA
+            hum = NA
+            pres = NA
         msg = "{'temp': '{}', 'hum': '{}', 'pres': '{}'  }".format(temp, hum, pres)
         client.publish(b"v1/devices/ESP32/telemetry", msg)
         time.sleep(30)
