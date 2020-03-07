33083c4c07a6423d08ac204a3355321e55d70357
Andrii Rykhlivskyi <dumpandrew@gmail.com>
2020-03-07 11:11:17 +0000

    Pass sensor id as MAC address

--- app/app.py
+++ app/app.py
@@ -6,6 +6,9 @@ import json
 import utime as time
 import app.bme280v2 as bme280
 
+import network
+import ubinascii
+
 client = MQTTClient("esp32-01", "iot.korivka.net")
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
 
@@ -16,11 +19,12 @@ def publish():
             temp = bme.temperature
             hum = bme.humidity
             pres = bme.pressure
+            cli = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
         except:
             temp = "NA"
             hum = "NA"
             pres = "NA"
-        msg = json.dumps(dict(temp=temp, hum=hum, pres=pres))
+        msg = json.dumps(dict(temp=temp, hum=hum, pres=pres, cli=cli))
         print(msg)
         client.publish(b"v1/devices/ESP32/telemetry", msg)
         time.sleep(30)
