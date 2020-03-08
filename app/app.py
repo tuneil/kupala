3955f958c5d2c644749ea6ffe5999a3840d24510
Andrii Rykhlivskyi <dumpandrew@gmail.com>
2020-03-08 14:07:11 +0000

    Fix issue with MQTT reconnect when service is unreachable

--- app/app.py
+++ app/app.py
@@ -13,22 +13,21 @@ client = MQTTClient("esp32-01", "iot.korivka.net")
 i2c = I2C(scl=Pin(22), sda=Pin(21), freq=10000)
 
 def publish():
-    while True:
-        try:
+    try:
             bme = bme280.BME280(i2c=i2c)
             temp = bme.temperature
             hum = bme.humidity
             pres = bme.pressure
             cli = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
-        except:
+    except:
             temp = "NA"
             hum = "NA"
             pres = "NA"
-        msg = json.dumps(dict(temp=temp, hum=hum, pres=pres, cli=cli))
-        print(msg)
-        client.publish(b"v1/devices/ESP32/telemetry", msg)
-        time.sleep(30)
+    msg = json.dumps(dict(temp=temp, hum=hum, pres=pres, cli=cli))
+    print(msg)
+    client.publish(b"v1/devices/ESP32/telemetry", msg)
+    time.sleep(30)
 
-client.reconnect()
-
-publish()
+while True:
+   client.reconnect()
+   publish()
